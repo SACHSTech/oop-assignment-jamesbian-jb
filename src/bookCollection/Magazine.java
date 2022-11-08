99100624b457d1287e7497697fd6b7b72b326da3
James Bian <james.bian23@ycdsbk12.ca>
2022-11-08 15:11:10 -0500

    worked on it

--- src/bookCollection/Magazine.java
+++ src/bookCollection/Magazine.java
@@ -2,10 +2,18 @@ package bookCollection;
 
 public class Magazine extends Book{
 
+    private String extraFeature;
 
-    public Magazine(String theTitle, String theGenre, String theAuthor, int publishDate, double theDecimal, boolean hasOnline,int numCopies) {
+
+    public Magazine(String theTitle, String theGenre, String theAuthor, int publishDate, double theDecimal, boolean hasOnline,int numCopies, String extraContent) {
+
         super(theTitle, theGenre, theAuthor, publishDate, theDecimal, hasOnline, numCopies);
+        extraFeature = extraContent;
 
+    }
+
+    public String getExtraFeature() {
+        return extraFeature;
     }
 
 }
